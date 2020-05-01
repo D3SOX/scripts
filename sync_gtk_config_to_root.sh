1c497a49df9bce9da0428f5660d5d2fc20b7e419
D3S0X <24937357+D3S0X@users.noreply.github.com>
2020-05-01 22:16:20 +0200

    Create gtk3 folder for root if it does not exist

--- sync_gtk_config_to_root.sh
+++ sync_gtk_config_to_root.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo mkdir -p /root/.config/gtk-3.0
 sudo cp ~/.config/gtk-3.0/settings.ini /root/.config/gtk-3.0/
 sudo cp ~/.gtkrc-2.0 /root/
 echo "Successfully copied $USER's GTK config to root"
