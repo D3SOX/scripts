3939fddb7f1d2e7788eec87d140a73655aab2641
D3SOX <nico@d3sox.me>
2022-07-11 17:55:13 +0200

    adjust firefox update script to ask for sudo
    I want to be able to instantly type my password with topgrade.
    This way it elevates when a parameter is given, see https://github.com/D3SOX/dotfiles/blob/master/topgrade/.config/topgrade.toml

--- update-firefox-theme.sh
+++ update-firefox-theme.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+if [ $# -eq 1 ]; then
+    # to ask for pw directly with topgrade
+    sudo echo
+fi
+
 PROFILE="xn2ithkq.Private"
 cd "$HOME/.mozilla/firefox/$PROFILE/chrome"
 for D in *; do [ -d "$D" ] && git -C "$D" pull; done
