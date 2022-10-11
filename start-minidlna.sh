9fc1b4e6d45cc8553d1a7c560da7a38308851a03
D3SOX <nico@d3sox.me>
2022-10-11 11:04:23 +0200

    delete minidlna scripts

--- start-minidlna.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-$(dirname $0)/stop-minidlna.sh
-minidlnad -f $HOME/.config/minidlna/minidlna.conf -P $HOME/.config/minidlna/minidlna.pid
--- stop-minidlna.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-killall minidlnad
-rm -rf $HOME/.cache/minidlna
