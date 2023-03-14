656c2cf686bfafd246c9f540bcd4a7b660e429e9
D3SOX <nico@d3sox.me>
2023-03-14 22:23:27 +0100

    add paru rebuild flag for rebuild kde pkgs script

--- rebuild-kde-aur-packages.sh
+++ rebuild-kde-aur-packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-paru -Qqem | grep kde | paru -S --noconfirm -
-paru -Qqem | grep plasma | paru -S --noconfirm -
-paru -Qqem | grep kwin | paru -S --noconfirm -
+paru -Qqem | grep kde | paru -S --rebuild --noconfirm -
+paru -Qqem | grep plasma | paru -S --rebuild --noconfirm -
+paru -Qqem | grep kwin | paru -S --rebuild --noconfirm -
