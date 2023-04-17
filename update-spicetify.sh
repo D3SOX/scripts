43e58de2eeeff39fd1059da441fb9f70ebbeb30c
D3SOX <nico@d3sox.me>
2023-04-17 17:31:25 +0200

    add too for spicetify script

--- update-spicetify.sh
+++ update-spicetify.sh
@@ -11,5 +11,6 @@ pull_git_dirs
 cd "$(dirname "$(spicetify -c)")/CustomApps"
 pull_git_dirs
 
+# TODO: only apply when something was updated (https://stackoverflow.com/a/3278427)
 spicetify apply -n
 exit 0
