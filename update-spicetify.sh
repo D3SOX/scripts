4dd7c7e8859097295f3dbf48a6b9f59c4946a738
D3SOX <nico@d3sox.me>
2023-08-13 22:56:03 +0200

    update spicetify updater script

--- update-spicetify.sh
+++ update-spicetify.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
 pull_git_dirs () {
-    for D in *; do [ -d "$D/.git" ] && git -C "$D" pull; done
+    for D in *; do [ -d "$D/.git" ] && printf "%b" "\e[1;33m==> DEBUG: \e[0mNow pulling $D\n" && git -C "$D" pull; done
 }
 
-cd "$(dirname "$(spicetify -c)")/Extensions"
+failed () {
+    printf "%b" "\e[1;31m==> ERROR: \e[0mFailed to find spicetify directory.\n"
+    exit 1
+}
+
+SPICETIFY_DIR="$(dirname "$(spicetify -c)")"
+
+printf "%b" "\e[1;33m==> DEBUG: \e[0mSpicetify directory is $SPICETIFY_DIR\n"
+
+if [ ! -d "$SPICETIFY_DIR" ]; then
+    failed
+fi
+
+printf "%b" "\e[1;34m==> INFO: \e[0mNow pulling extensions\n"
+cd "$SPICETIFY_DIR/Extensions" || failed
 pull_git_dirs
-cd "$(dirname "$(spicetify -c)")/Themes"
+printf "%b" "\e[1;34m==> INFO: \e[0mNow pulling themes\n"
+cd "$SPICETIFY_DIR/Themes" || failed
 pull_git_dirs
-cd "$(dirname "$(spicetify -c)")/CustomApps"
+printf "%b" "\e[1;34m==> INFO: \e[0mNow pulling custom apps\n"
+cd "$SPICETIFY_DIR/CustomApps" || failed
 pull_git_dirs
 
+printf "%b" "\e[1;92m==> SUCCESS: \e[0mDone. Running 'spicetify apply -n'\n"
 # TODO: only apply when something was updated (https://stackoverflow.com/a/3278427)
 spicetify apply -n
 exit 0
